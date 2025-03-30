a93681693fb2dd416c2dd3d83c326ad4aafc2cc2
esteban1318 <poveda1318@gmail.com>
2025-03-30 17:48:28 -0500

    Agregado login con validación y redirección a principal.html

--- js/login.js
+++ js/login.js
@@ -3,19 +3,21 @@ console.log("login.js cargado");
 const usuario = "admin";
 const contraseña = "1234";
 
-function validarLogin(){
-    let user=document.getElementById("inpuText").value.trim();
-    let pass=document.getElementById("contraseña").value.trim();
-    let mensaje=document.getElementById("mensaje");
+function validarLogin() {
+    let user = document.getElementById("inputText").value.trim();
+    let pass = document.getElementById("contraseña").value.trim();
+    let mensaje = document.getElementById("mensaje");
 
-    if(user===usuario && pass===contraseña){
-        mensaje.textContent="login exitoso";
-        setTimeout(()=>{
-            window.location.href="index.html";
-        },200);
-       
-    }else{
-        mensaje.textContent="usuario o contraseña incorrecta";
+    if (user === usuario && pass === contraseña) {
+        mensaje.textContent = "Login exitoso";
 
+        // Guardar sesión en localStorage
+        localStorage.setItem("usuarioLogueado", "true");
+
+        setTimeout(() => {
+            window.location.href = "principal.html";
+        }, 200);
+    } else {
+        mensaje.textContent = "Usuario o contraseña incorrecta";
     }
 }
